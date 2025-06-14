eb1d562a9610bb562f2b73a008718b6ed656f656
pitzachef <johnathansrfve@gmail.com>
2025-06-14 14:56:46 +0800

    Update linganguliguliguliwatanglinganggooolinganggoolingangguligulliwatanglingangulingagu.lua

--- linganguliguliguliwatanglinganggooolinganggoolingangguligulliwatanglingangulingagu.lua
+++ linganguliguliguliwatanglinganggooolinganggoolingangguligulliwatanglingangulingagu.lua
@@ -1,1 +1,14 @@
 
+-- asset definition
+
+
+
+if isfolder("spotify") then
+
+getcustomasset("spotify/assets/")
+
+else 
+    makefolder("spotify")
+    makefolder("spotify/assets")
+    makefolder("spotify/api")
+end
